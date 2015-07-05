7589373a94655274407df97bb22475ddbe1fb7cd
Luke Bayes <lukebayes@gmail.com>
2015-07-04 18:28:43 -0700

    Working on vim configuration

--- install.sh
+++ install.sh
@@ -3,6 +3,28 @@ set -io pipefail
 
 cd $HOME
 dotfiles="$HOME/src/dotfiles"
+force=""
+
+# Show help message
+show_help() {
+  cat << EOF
+
+Usage: ${0##*/} [-f --help]
+Configure a home directory for development.
+
+  -f          Force or overwrite existing files and directories
+  --help   Show this message
+EOF
+}
+
+# Process command line options
+while getopts "f" opt; do
+  case "$opt" in
+    h) show_help exit 0;;
+    f) force="-f";;
+    ?) show_help exit 1;;
+  esac
+done
 
 if [ ! -f $HOME/.ssh/github_rsa.pub ]; then
   echo "ERROR: You must have an SSH public key at $HOME/.ssh/github_rsa.pub"
@@ -16,10 +38,11 @@ mkdir -p "$dotfiles"
 # git clone https://github.com/lukebayes/dotfiles.git $dotfiles
 
 echo "Creating symlinks to $HOME from $dotfiles"
-ln -fs $dotfiles/home/gitignore .gitignore
-ln -fs $dotfiles/home/gitconfig .gitconfig
 
+# Copy all files in dotfiles/home/* to $HOME/.[filename]
 for file in $dotfiles/home/*
 do
-  echo "FILE: $file"
+  echo "ln $force -s $file \"$HOME/.${file##*/}\""
+  ln $force -s $file "$HOME/.${file##*/}"
 done
+
