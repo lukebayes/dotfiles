d68ed170292f6e7cf0e4f066c00b227b5ffa00d7
Luke Bayes <lukebayes@gmail.com>
2015-07-04 19:43:25 -0700

    Working on themes

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -io pipefail
 
+SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Running installation from $SOURCEDIR"
+exit 0
+
 cd $HOME
-dotfiles="$HOME/src/dotfiles"
 force=""
+skipapt=false
 skipvim=false
 
 # Show help message
@@ -14,8 +19,9 @@ Usage: ${0##*/} [-force --help --skip-vim]
 Configure a home directory for development.
 
   -f|--force      Force or overwrite existing files and directories
-  --skip-vim  Skip vim configuration
-  -h|--help   Show this message
+  --skip-apt      Skip apt-get update and installation
+  --skip-vim      Skip vim configuration
+  -h|--help       Show this message
 EOF
 }
 
@@ -23,9 +29,10 @@ EOF
 while [ $# -gt 0 ]; do
   case $1 in
     -f|--force)   force="-f";       shift  ;;
+    --skip-apt)   skipapt=true;     shift  ;;
     --skip-vim)   skipvim=true;     shift  ;;
-    -h|--help)    show_help;         exit 0;;
-    *)            show_help;         exit 1;;
+    -h|--help)    show_help;        exit 0 ;;
+    *)            show_help;        exit 1 ;;
   esac
 done
 
@@ -34,21 +41,42 @@ if [ ! -f $HOME/.ssh/github_rsa.pub ]; then
   exit 1
 fi
 
-echo "Creating dotfiles directory at $dotfiles"
-mkdir -p "$dotfiles"
+echo "Creating dotfiles directory at $SOURCEDIR"
+mkdir -p "$SOURCEDIR"
 
 # Enable as final step
-# git clone https://github.com/lukebayes/dotfiles.git $dotfiles
+# git clone https://github.com/lukebayes/dotfiles.git $SOURCEDIR
 
-echo "Creating symlinks to $HOME from $dotfiles"
+echo "Creating symlinks to $HOME from $SOURCEDIR"
 
 # Copy all files in dotfiles/home/* to $HOME/.[filename]
-for file in $dotfiles/home/*
+for file in $SOURCEDIR/home/*
 do
   echo "ln $force -s $file \"$HOME/.${file##*/}\""
   ln $force -s $file "$HOME/.${file##*/}"
 done
 
+if [ $skipapt != false ] && [ ! shopt -oq posix ]; then
+# Install binaries
+sudo apt-get update && \
+  sudo apt-get install \
+  git-core \
+  python-pip
+fi
+
+# Upgrade pip
+pip install -U pip
+pip install --user git+git://github.com/lukebayes/powerline
+pip install git+git://github.com/lukebayes/powerline
+
+wget https://github.com/lukebayes/powerline/raw/develop/font/PowerlineSymbols.otf \
+  https://github.com/lukebayes/powerline/raw/develop/font/10-powerline-symbols.conf
+
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo fc-cache -vf
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+
+# Configure VIM
 if [ $skipvim = false ]; then
   mkdir -p $HOME/.vim/bundle
   if [ -e "$HOME/.vim/bundle/vundle" ] && [ $force == "-f" ]; then
@@ -61,4 +89,9 @@ if [ $skipvim = false ]; then
   # echo "Installing provided VIM plugins"
   vim -c "PluginInstall!" -c "q" -c "q"
 fi
+
+# Set up the Tomorrow Night shell theme
+./$SOURCEDIR/setup-theme.sh
+
+source $HOME/.bashrc
 
